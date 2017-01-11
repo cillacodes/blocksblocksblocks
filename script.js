fe9edb54a2a4ac7225572e05e4b8247e1355af3e
Priscilla <Priscillayg23@gmail.com>
2017-01-11 15:44:02 -0600

    add delete button to divs

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
 
   $('button').click(function() {
-  $('#appendDiv').append('<div class="box"><div>');
+  $('#appendDiv').append('<div class="box"><button class="deleteMe">X</button><div>');
+  $('.box:last').css('backgroundColor', randomColors())
 });
+
 
 $('#appendDiv').on("click", ".box", function() {
   $(this).css({backgroundColor: "black" });
 });
 
+$('#appendDiv').on('click', '.deleteMe', function() {
+  $(this).closest('.box').remove();
+});
+
 
 });
+
+var colors = ['00', '33', '66', '99', 'cc', 'ff'];
+var random = function() {
+  return Math.floor(Math.random()*6);
+};
+
+var randomColors = function() {
+  var r = colors[random()];
+  var g = colors[random()];
+  var b = colors[random()];
+  return "#"+r+g+b;
+};
